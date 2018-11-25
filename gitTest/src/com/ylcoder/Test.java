89f9b92cffdf0b391856a26a3798f0b05c83f1b2
ylcoder <ylcoder@qq.com>
2018-11-25 19:48:23 +0800

    git_idea项目测试

---
+++ gitTest/src/com/ylcoder/Test.java
@@ -0,0 +1,15 @@
+package com.ylcoder;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
